ea26fb4e887bd11002e2ee66e117c06c55e5b851
digitalrehman <abdulrehmanarain1018@gmail.com>
2025-10-18 07:20:39 -0700

    complete ui

---
+++ src/view/src/components/DialogDemo.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function DialogDemo() {
+    return (
+        <Dialog>
+            <form>
+                <DialogTrigger asChild>
+                    <Button variant="outline">Open Dialog</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Edit profile</DialogTitle>
+                        <DialogDescription>
+                            Make changes to your profile here. Click save when you&apos;re
+                            done.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4">
+                        <div className="grid gap-3">
+                            <Label htmlFor="name-1">Name</Label>
+                            <Input id="name-1" name="name" />
+                        </div>
+                        <div className="grid gap-3">
+                            <Label htmlFor="email">Email</Label>
+                            <Input id="email" type="email" name="email" />
+                        </div>
+                        <div className="grid gap-3">
+                            <Label htmlFor="password">Password</Label>
+                            <Input id="password" type="email" name="password" />
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="outline">Cancel</Button>
+                        </DialogClose>
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </form>
+        </Dialog>
+    )
+}
